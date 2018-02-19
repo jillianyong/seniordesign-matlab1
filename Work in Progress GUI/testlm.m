b8037b4d1a3f3cb9e6acccca1bf8460c241fbf25
max christ <antimonkey@optonline.net>
2018-02-19 13:47:13 -0500

    test the lm function

---
+++ Work in Progress GUI/testlm.m
@@ -0,0 +1,17 @@
+%Import Table
+x = detectImportOptions('BCP_Activity6021.xlsx');
+T = readtable('BCP_Activity6021.xlsx',x);
+
+mdl = fitlm(T,'mean_liver_fat_p~waist_cir3+bmi3+diabetes_type+total_fat_index+ActivityIndex')
+
+%Imaginary Test Subject
+waist_cir3 = 90;
+bmi3 = 30;
+diabetes_type = 2;
+total_fat_index = 4;
+ActivityIndex = -1;
+newT = table(waist_cir3, bmi3, diabetes_type, total_fat_index, ActivityIndex);
+
+%Prediction of liver fat and confidence interval
+[livfat, conint] = predict(mdl, newT)
+
