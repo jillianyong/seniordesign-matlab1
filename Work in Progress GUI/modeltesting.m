64834326e6e142cfb4ca1dd0c00907a19f24ad86
rychen58 <32885235+rychen58@users.noreply.github.com>
2018-02-21 20:43:31 -0500

    Update modeltesting.m

--- Work in Progress GUI/modeltesting.m
+++ Work in Progress GUI/modeltesting.m
@@ -12,7 +12,23 @@ T3 = T(1501:4521,:);
 %lm2 = fitlm(T2,'mean_liver_fat_p~age3+sex+ActivityIndex+SAT_index+VAT_index+diabetes_type');
 %lm3 = fitlm(T3,'mean_liver_fat_p~age3+sex+ActivityIndex+SAT_index+VAT_index+diabetes_type');
 
-mdl = fitlm(T,'mean_liver_fat_p~sex+waist_cir3+bmi3+diabetes_type+total_fat_index+Activity')
-mdl1 = fitlm(T1,'mean_liver_fat_p~sex+waist_cir3+bmi3+diabetes_type+total_fat_index+Activity')
-mdl2 = fitlm(T2,'mean_liver_fat_p~sex+waist_cir3+bmi3+diabetes_type+total_fat_index+Activity')
-mdl3 = fitlm(T3,'mean_liver_fat_p~sex+waist_cir3+bmi3+diabetes_type+total_fat_index+Activity')
+% Eitehr Avtivity or ActivityIndex
+% Prediction for Mean liver fat percentage
+%mlfp = fitlm(T,'mean_liver_fat_p~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex')
+%mlfp1 = fitlm(T1,'mean_liver_fat_p~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex')
+%mlfp2 = fitlm(T2,'mean_liver_fat_p~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex')
+%mlfp3 = fitlm(T3,'mean_liver_fat_p~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex')
+
+% Prediction for Inverted squared liver fat 
+%islf = fitlm(T,'inv_sqrt_liver_fat~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex')
+%islf1 = fitlm(T1,'inv_sqrt_liver_fat~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex')
+%islf2 = fitlm(T2,'inv_sqrt_liver_fat~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex')
+%islf3 = fitlm(T3,'inv_sqrt_liver_fat~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex')  
+
+% Prediction for Liver fat transform
+lft = fitlm(T,'liver_fat_transform~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex+VAT_index')
+lft1 = fitlm(T1,'liver_fat_transform~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex+VAT_index')
+lft2 = fitlm(T2,'liver_fat_transform~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex+VAT_index')
+lft3 = fitlm(T3,'liver_fat_transform~age3+waist_cir3+bmi3+diabetes_type+ActivityIndex+VAT_index')  
+
+
